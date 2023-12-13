b81d01f57586b7de4c649f0b0f4ed084230a8ac5
Guy Dvorkin <dvorkin.guy@gmail.com>
2023-12-13 18:18:17 +0200

    edited

--- main.py
+++ main.py
@@ -3,15 +3,11 @@ calculation_to_hours = 24
 name_of_unit = 'hours'
 
 
-def days_to_units (number_of_days, custom_message):
+def days_to_units (number_of_days):
     print(f"{number_of_days} days are {number_of_days * calculation_to_hours} {name_of_unit}.")
-    print(custom_message)
 
 
-def scope_check(num_of_days):
-    print(name_of_unit)
-    print(num_of_days)
+my_var = days_to_units(20)
 
-
-days_to_units(2, "as")
-scope_check(2)
+user_input = input("Hey user, input days, and i will convert it to hours!\n")
+print(user_input)
