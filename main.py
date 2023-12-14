b4b6844a1d7e4c58a225f0633a71706bd9928f3d
Guy Dvorkin <dvorkin.guy@gmail.com>
2023-12-14 14:13:12 +0200

    sets

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ def days_to_units (number_of_days):
 def validate_and_execute():
     try:
         user_input_number = int(num_of_days_element)
+
+        # We want to do conversion only for positive integers
         if user_input_number > 0:
             calculated_value = days_to_units(int(num_of_days_element))
             print(calculated_value)
@@ -23,10 +25,15 @@ def validate_and_execute():
     except:
         print("\nYour input isn't a valid number. Don't ruin my program!\n\n")
 
+
 user_input = ""
 while user_input != "exit":
-    user_input = input("\nHey, provide me number of days as a comma separated list, I'll convert it to hours!\n\n")
-    print(type(user_input.split(", ")))
-    print(user_input.split(", "))
-    for num_of_days_element in user_input.split(", "):
+    user_input = input("\nHey, enter a number of days and convestion unit!\n\n")
+    #print(type(user_input.split(", ")))
+    #print(user_input.split(", "))
+    print(type(set(user_input.split(", "))))
+    
+    for num_of_days_element in set(user_input.split(", ")):
         validate_and_execute()
+
+
--- test.py
+++ test.py
@@ -1,29 +1,8 @@
-calculation_to_hours = 24
-name_of_unit = 'hours'
+text = "Hello World"
+lower_letter = sum(1 for char in text if char.upper())
+print(type(lower_letter))
+print(lower_letter)
+print(text)
 
-
-def days_to_units(number_of_days):
-    return f"\nGreat, {number_of_days} days are {number_of_days * calculation_to_hours} {name_of_unit}.\n"
-
-
-def validate_and_execute():
-    try:
-        user_input_number = int(num_of_days_element)
-        if user_input_number > 0:
-            # Corrected this line to use the correct variable 'num_of_days_element'
-            calculated_value = days_to_units(int(num_of_days_element))
-            print(calculated_value)
-        elif user_input_number == 0:
-            print("\nYou have entered 0. Kindly input a positive number.\n\n")
-        else:
-            print("\nYou have entered a negative number. Just stop doing it!\n\n")
-    
-    except:
-        print("\nYour input isn't a valid number. Don't ruin my program!\n\n")
-
-user_input = ""
-while user_input != "exit":
-    user_input = input("\nHey, provide me with some number!\n")
-    for num_of_days_element in user_input.split():
-        # Updated the function call to pass the correct variable 'num_of_days_element'
-        validate_and_execute()
+[1,2,3].insert()
+"2, 3".split()
