79a1fe65aa671155456d12e3a9d5efd9218d1fbd
Guy Dvorkin <dvorkin.guy@gmail.com>
2023-12-14 09:51:03 +0200

    standart

--- main.py
+++ main.py
@@ -4,20 +4,26 @@ name_of_unit = 'hours'
 
 
 def days_to_units (number_of_days):
-    conditional_check = number_of_days > 0
-    print(type(conditional_check))
-
-    if number_of_days > 0:
-        return (f"\nGreat, {number_of_days} days are {number_of_days * calculation_to_hours} {name_of_unit}.\n")
-    elif number_of_days == 0:
-        return (f"\nYou have entered a 0. Kindly input a positive number.\n")
-    else:
-        return "\nYou have entered a negative value. Unble to calculate.\n"
+    return (f"\nGreat, {number_of_days} days are {number_of_days * calculation_to_hours} {name_of_unit}.\n")
+    
 
 
-user_input = input("\nHey, provide me with some number!\n\n")
-user_input_number = int(user_input)
+def validate_and_execute():
+    try:
+        user_input_number = int(user_input)
+        if user_input_number > 0:
+            calculated_value = days_to_units(int(user_input))
+            print(calculated_value)
+        elif user_input_number == 0:
+            print("\nYou have entered a 0. Kindly input a positive number.\n\n")
+        else:
+            print("\nYou have enetered a negative number. Just stop doing it!\n\n")
+    
+    except:
+        print("\nYour input isn't a valid number. Don't ruin my program!\n\n")
 
-calculated_value = days_to_units(int(user_input))
-print(calculated_value)
-
+user_input = ""
+while user_input != "exit":
+    user_input = input("\nHey, provide me with some number!\n")
+    for number_of_days in user_input:
+        validate_and_execute()
